1d92414cab3d96413e77bc519f8579bbad2e472f
Sebastian Garcia <eldraco@gmail.com>
2022-09-05 18:06:17 -0300

    Read the folders and list files

--- merge-zeek-files.py
+++ merge-zeek-files.py
@@ -7,100 +7,87 @@
 
 import argparse
 from datetime import datetime
+import os
+from os import listdir
+from os.path import isfile, join
 
+# How it works
+# - Read the folders with files. The first folder has the 'base' files. The second has the files to be inserted in the first.
+# - Create the temp folder and the temp files
+# - Merge the conn file according to the rules of merging
+# - While merging the conn.log file, for each flow also merge the rest of the alternative flows from the other files
+# - For each of the files in the second folder do
+# - Find the uid in the conn.log file
+# - Search the corresponding alternative flows (ssl, ssh, etc.)
+# - Move the alternative flows to the same time as the conn.log flow
+# - For the files.log and x509.log files, that don't have a uid, first read those
 
-def print_histogram_amount_file(zeekfile, bin_size):
+def merge_conn_files(firstfile, secondfile):
     """
-    Get a file, read it, count the amount of flows, print a histogram
-    bin_size is in hours 
+    Insert the second zeek log file into the first zeek file.
+    Steps
+    1. Read the time of the first flow of the first file
+    1. Read the time of the last flow of the first file
+    2. Read the time of the first flow of the second file
+    1. Read the time of the last flow of the second file
+    3. Compute the difference
+    4. Compute the new start of the second file: diff - time asked. So if diff = 4hs and time asked to move is 2hs, then we need to move -2hs
+    5. Create new output file with the content of the first file
+    6. Add lines of the second file to the output file (out of order) but only if the are < than the time of the last flow of the first file
+    7. Sort the output file with | sort
+    8. 
+    """
+    pass
+
+
+def create_temp_folder_and_files(firstfolder):
+    """
+    Create the temp folder and files
     """
     if args.debug > 0:
-        print(f'[debug] Processing file {zeekfile}, bin={bin_size}')
-    bins_data = {}
-    max_value = 0
-    # Bins step is in seconds since ts is in seconds
-    bins_step = 3600 * bin_size
-    with open(zeekfile, 'r') as f:
-        line = f.readline()
-        # loop to ignore comments
-        while line and line[0] == '#':
-            line = f.readline()
-        start_of_bin = False
-        end_of_bin = False
-        while line:
-            # Process flows
-            flow_time = float(line.split('\t')[0])
-            if args.debug > 0:
-                print(f'New flow: {flow_time}. Current start of bin:{start_of_bin}. Current end:{end_of_bin}')
-            if not start_of_bin:
-                start_of_bin = flow_time
-                end_of_bin = start_of_bin + bins_step
-                bins_data[start_of_bin] = 1
-                if args.debug > 0:
-                    print(f'\t[+] Start. ts={flow_time}. Start of bin={start_of_bin}. End of bin={end_of_bin}. Data:{bins_data[start_of_bin]}')
-            elif flow_time <= end_of_bin:
-                bins_data[start_of_bin] += 1
-                if args.debug > 0:
-                    print(f'\t[+] Add Flow. ts={flow_time}. Start of bin={start_of_bin}. End of bin={end_of_bin}. Data:{bins_data[start_of_bin]}')
-                if bins_data[start_of_bin] > max_value:
-                    max_value = bins_data[start_of_bin]
-            elif flow_time > end_of_bin:
-                start_of_bin = end_of_bin
-                end_of_bin = start_of_bin + bins_step
-                bins_data[start_of_bin] = 1
-                if args.debug > 0:
-                    print(f'\t[+] New bin. ts={flow_time}. Start of bin={start_of_bin}. End of bin={end_of_bin}. Data: {bins_data[start_of_bin]}')
-            elif flow_time < start_of_bin:
-                print(f'Oh no. A flow out of order. Please sort first')
-                return True
-            line = f.readline()
-            # loop to ignore comments at the end
-            while line and line[0] == '#':
-                line = f.readline()
-    # Print histogram
-    if bins_data:
-        print(f'Histogram of flows in the zeek file {zeekfile}. Bin size:{bin_size}hs\n')
-        prev_key = False
-        for key in bins_data:
-            hkey = datetime.fromtimestamp(float(key))
-            if not prev_key:
-                prev_key = key
-                prev_hkey = datetime.fromtimestamp(float(prev_key))
-            else:
-                line_size = int(bins_data[prev_key] / max_value * 100)
-                asterics = '*' * line_size
-                print(f'{prev_hkey} - {hkey}: {bins_data[prev_key]} {asterics}')
-                prev_key = key
-                prev_hkey = datetime.fromtimestamp(float(prev_key))
-        final_key = prev_key + bins_step
-        final_hkey = datetime.fromtimestamp(float(final_key))
-        line_size = int(bins_data[prev_key] / max_value * 100)
-        asterics = '*' * line_size
-        print(f'{prev_hkey} - {final_hkey}: {bins_data[prev_key]} {asterics}')
-    else:
-        print('The log file did not have any flows')
+        print(f'Creating the temp folders and files')
+
+    merged_first_folder = firstfolder + '-merged'
+    try:
+        os.mkdir(merged_first_folder)
+        if args.debug > 0:
+            print(f'Created: {merged_first_folder}')
+    except FileExistsError:
+        # do nothing, just use it
+        if args.debug > 0:
+            print(f'Merged folder already existed')
+
+    # List files
+    # TODO ignore everything that does not end in .log
+    files_firstfolder = [f for f in listdir(firstfolder) if isfile(join(firstfolder, f))]
+
+    for files in files_firstfolder:
+        f = open()
+
+
+
+
+def merge_folders(firstfolder, secondfolder):
+    """
+    Read the two folders
+    """
+
+    create_temp_folder_and_files(firstfolder)
 
 
 # Main
 ####################
 if __name__ == '__main__':  
-    print(f'Zeek logs merger and analyser')
-    print('Zeek flows MUST be sorted!\n')
+    print(f'Zeek logs merger. Inserts the second file in the first file.')
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', help='Amount of verbosity. This shows more info about the results.', action='store', required=False, type=int, default=1)
     parser.add_argument('-d', '--debug', help='Amount of debugging. This shows more info about inner working.', action='store', required=False, type=int, default=0)
-    parser.add_argument('-1', '--firstfile', help='Name of the base Zeek file where you want to insert into.', action='store', required=False, type=str)
-    parser.add_argument('-2', '--secondfile', help='Name of the Zeek file that you want to be inserted into the base Zeek file.', action='store', required=False, type=str)
-    parser.add_argument('-H', '--histogram', help='Do a histogram of amount of flows per bin in this Zeek file.', action='store', required=False, type=str)
-    parser.add_argument('-b', '--histogrambinsize', help='Use this bin size in the histrogram. In number of hours. Defaults to 1.', action='store', required=False, type=float, default=1)
+    parser.add_argument('-1', '--firstfolder', help='Name of the base Zeek folder where you want to insert flows into.', action='store', required=True, type=str)
+    parser.add_argument('-2', '--secondfolder', help='Name of the Zeek folder that you want to be inserted into the base Zeek folder.', action='store', required=True, type=str)
 
     args = parser.parse_args()
 
-    if args.firstfile and args.secondfile:
-        # print(f'first {args.firstfile}, second {args.secondfile}')
-        pass
+    if args.verbose > 0:
+        print(f'Merging folder {args.secondfolder} into folder {args.firstfolder}')
 
-    elif args.histogram:
-        print_histogram_amount_file(zeekfile=args.histogram, bin_size=args.histogrambinsize)
-
-
+    merge_folders(args.firstfolder, args.secondfolder)
